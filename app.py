a1d3819accadb0a4d52cff8ee31d929d9fbf4882
jh000107 <junhuicho8@gmail.com>
2023-12-12 19:28:26 -0500

    finalized

--- app.py
+++ app.py
@@ -99,13 +99,109 @@ def logout():
 
 @app.route("/")
 def index():
-    return "Hello World <a href='/login'><button>Login</button></a>"
+    return f"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <style>
+        body {{
+            background-image: url('/static/images/background.jpeg'); 
+            background-size: cover; 
+            background-position: center; 
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh; 
+            margin: 40px;
+        }}
+        p {{
+            font-size: 48px; 
+            margin: 20px 0; 
+        }}
+        .pretty-button {{
+            background-color: #4CAF50; 
+            border: none;
+            color: white; 
+            padding: 15px 60px; 
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 28px; 
+            margin: 20px 5px; 
+            cursor: pointer;
+            border-radius: 8px; 
+            transition-duration: 0.4s; 
+        }}
+
+        .pretty-button:hover {{
+            background-color: white; 
+            color: black; 
+        }}
+    </style>
+    </head>
+    <body>
+        <p>Welcome to Our Innovative Learning Platform </p>
+        <a href='/login'><button class='pretty-button'>Login</button></a>
+    </body>
+    </html>
+    """
+    
 
 
 @app.route("/protected_area")
 @login_is_required
 def protected_area():
-    return f"Hello {session['name']}! <br/> <a href='/gradioThread'><button>Gradio</button></a> <br/> <a href='/logout'><button>Logout</button></a> <br/> <a href='/register'><button>Register</button></a>"
+    return f"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <style>
+        body {{
+            background-image: url('/static/images/background.jpeg'); 
+            background-size: cover; 
+            background-position: center; 
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh; 
+            margin: 40px;
+        }}
+        p {{
+            font-size: 36px; 
+            margin: 20px 0; 
+        }}
+        .pretty-button {{
+            background-color: #4CAF50; 
+            border: none;
+            color: white; 
+            padding: 15px 60px; 
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 28px;
+            margin: 20px 5px; 
+            cursor: pointer;
+            border-radius: 8px; 
+            transition-duration: 0.4s; 
+        }}
+
+        .pretty-button:hover {{
+            background-color: white; 
+            color: black; 
+        }}
+    </style>
+    </head>
+    <body>
+        <p>Welcome {session['name']}!</p>
+        <a href='/gradioThread'><button class='pretty-button'>Gradio</button></a>
+        <a href='/logout'><button class='pretty-button'>Logout</button></a>
+        <a href='/register'><button class='pretty-button'>Register</button></a>
+    </body>
+    </html>
+    """
+
 
 # @app.route('/gradio', methods=['GET'])
 # def gradio_app():
@@ -123,7 +219,11 @@ def home():
         <h1>Flask and Gradio Integration</h1>
         <iframe src="http://localhost:7860" width="100%" height="100%"></iframe>
         <br/> <a href='/logout'><button>Logout</button></a>
+        <a href='/protected_area'><button>Home Page</button></a>
+        
     ''')
+
+
 
 # threading.Thread(target=gradio_app, daemon=True).start()
 
@@ -132,7 +232,131 @@ def register():
     if request.method == 'POST':
         username = request.form['username']
         email = request.form['email']
-        # Handle other form fields
+
+        if not email.endswith('@gmail.com'):
+            return render_template_string('''
+        <html>
+        <head>
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    margin: 0;
+                    padding: 0;
+                    height: 100vh; 
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center; 
+                    align-items: center; 
+                    text-align: center;
+                    background-image: url('/static/images/background.jpeg'); 
+                    background-size: cover; 
+                    background-position: center; 
+                }
+                p {
+                    color: #333; 
+                    font-size = 48px;
+                    margin: 10px 0;
+                }
+                a {
+                    color: #007bff; 
+                    text-decoration: none; 
+                    margin: 10px 0; 
+                }
+                a:hover {
+                    text-decoration: underline;
+                }
+                
+                .pretty-button {
+            background-color: #4CAF50; 
+            border: none;
+            color: white; 
+            padding: 15px 60px; 
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 28px; 
+            margin: 20px 5px; 
+            cursor: pointer;
+            border-radius: 8px; 
+            transition-duration: 0.4s; 
+        }
+
+        .pretty-button:hover {
+            background-color: white; 
+            color: black; 
+        }
+            </style>
+        </head>
+        <body>
+            <p>Only Gmail addresses are accepted. Please provide a valid Gmail address.</p>
+            <a href="/register">Try again</a>
+        </body>
+        </html>
+    ''')
+        # Check if email already exists
+        existing_user = User.query.filter_by(email=email).first()
+        if existing_user:
+            # User with this email already exists
+            return render_template_string('''
+        <html>
+        <head>
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    margin: 0;
+                    padding: 0;
+                    height: 100vh; 
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                    align-items: center; 
+                    text-align: center;
+                    background-image: url('/static/images/background.jpeg'); 
+                    background-size: cover;
+                    background-position: center; 
+                }
+                p {
+                    color: #333; 
+                    font-size = 48px;
+                    margin: 10px 0; 
+                }
+                a {
+                    color: #007bff; 
+                    text-decoration: none; 
+                    margin: 10px 0; 
+                }
+                a:hover {
+                    text-decoration: underline; 
+                }
+                
+                .pretty-button {
+            background-color: #4CAF50; 
+            border: none;
+            color: white; 
+            padding: 15px 60px; 
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 28px;
+            margin: 20px 5px; 
+            cursor: pointer;
+            border-radius: 8px; 
+            transition-duration: 0.4s; 
+        }
+
+        .pretty-button:hover {
+            background-color: white; 
+            color: black; 
+        }
+            </style>
+        </head>
+        <body>
+            <p>This email is already registered. Please use a different email.</p>
+            <a href="/register">Try again</a>
+            <a href='/gradioThread'><button class='pretty-button'>Gradio</button></a>
+        </body>
+        </html>
+    ''')
 
         # Save to database
         new_user = User(username=username, email=email)
@@ -142,24 +366,134 @@ def register():
         return redirect(url_for('profile', user_id=new_user.id))
 
     return render_template_string('''
-        <form method="post">
-            Username: <input type="text" name="username"><br>
-            Email: <input type="email" name="email"><br>
-            <!-- Add other fields here -->
-            <input type="submit" value="Register">
-        </form>
-        <br/> <a href='/gradioThread'><button>Gradio</button></a>
+        <html>
+        <head>
+            <style>
+                body {
+                    font-family: Arial, sans-serif; 
+                    padding: 20px; 
+                    text-align: center;
+                    background-image: url('/static/images/background.jpeg'); 
+                    background-size: cover; 
+                    background-position: center; 
+                }
+                form {
+                    background-color: #f2f2f2; 
+                    padding: 20px; 
+                    border-radius: 8px; 
+                    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2); 
+                    margin-bottom: 20px; 
+                }
+                input[type=text], input[type=email] {
+                    width: 50%; 
+                    padding: 12px 20px; 
+                    margin: 8px 0; 
+                    display: inline-block; 
+                    border: 1px solid #ccc; 
+                    border-radius: 4px; 
+                    box-sizing: border-box; 
+                }
+                input[type=submit] {
+                    width: 50%;
+                    background-color: #4CAF50; 
+                    color: white; 
+                    padding: 14px 20px; 
+                    margin: 8px 0; 
+                    border: none;
+                    border-radius: 4px; 
+                    cursor: pointer; 
+                }
+                input[type=submit]:hover {
+                    background-color: #45a049; 
+                }
+                .pretty-button {
+            background-color: #4CAF50; 
+            border: none;
+            color: white; 
+            padding: 15px 60px; 
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 28px; 
+            margin: 20px 5px; 
+            cursor: pointer;
+            border-radius: 8px; 
+            transition-duration: 0.4s; 
+        }
+
+        .pretty-button:hover {
+            background-color: white; 
+            color: black; 
+        }
+                
+            </style>
+        </head>
+        <body>
+            <form method="post">
+                Username: <input type="text" name="username"><br>
+                Email: <input type="email" name="email"><br>
+                <input type="submit" value="Register">
+            </form>
+            <br/> <a href='/gradioThread'><button class='pretty-button'>Gradio</button></a>
+        </body>
+        </html>
     ''')
 
 @app.route('/profile/<int:user_id>')
 def profile(user_id):
     user = User.query.get_or_404(user_id)
+    
     return render_template_string('''
-        <h1>User Profile</h1>
-        <p>Username: {{ user.username }}</p>
-        <p>Email: {{ user.email }}</p>
-        <!-- Display other fields -->
-        <br/> <a href='/gradioThread'><button>Gradio</button></a>
+        <html>
+        <head>
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    margin: 0;
+                    padding: 0;
+                    height: 100vh; 
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center; 
+                    align-items: center; 
+                    text-align: center;
+                    background-image: url('/static/images/background.jpeg'); 
+                    background-size: cover; 
+                    background-position: center; 
+                }
+                h1 {
+                    color: #fff;
+                    text-shadow: 2px 2px 4px #000; 
+                }
+                p {
+                    color: #fff; 
+                    text-shadow: 1px 1px 3px #000; 
+                }
+                .pretty-button {
+                    background-color: #4CAF50; 
+                    color: white; 
+                    padding: 10px 20px; 
+                    border: none;
+                    border-radius: 5px; 
+                    font-size: 16px; 
+                    cursor: pointer; 
+                    margin-top: 15px; 
+                }
+                .pretty-button:hover {
+                    background-color: #45a049; 
+                }
+                a {
+                    text-decoration: none; 
+                }
+            </style>
+        </head>
+        <body>
+            <h1>User Profile</h1>
+            <p>Username: {{ user.username }}</p>
+            <p>Email: {{ user.email }}</p>
+            <br/> <a href='/gradioThread'><button class='pretty-button'>Gradio</button></a>
+        </body>
+        </html>
     ''', user=user)
 
 if __name__ == "__main__":
